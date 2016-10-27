270aea99d10b8a64b41aa7329890d4fea8845302
wfr9273 <wangfr573@gmail.com>
2016-10-27 11:35:47 -0700

    Create instruction.h

---
+++ Project-5/instruction.h
@@ -0,0 +1,23 @@
+#ifndef INSTRUCTION_H
+#define	INSTRUCTION_H
+
+#include <iostream>
+#include "word.h"
+
+using namespace std;
+
+class Instruction : public Word
+{
+  char *info;
+public:
+  Instruction(int addr);
+  ~Instruction();
+  const char* getInfo() const;
+  void setInfo(const char* information);
+  void operator=(const char* information);
+  friend ostream& operator<< (ostream &os, const Instruction &instruction);
+  
+}; //class Instruction
+
+#endif	// INSTRUCTION_H 
+
